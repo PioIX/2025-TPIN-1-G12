3ddef889485c24813decd0c69f135941d65a9bfd
segifer08 <sgilfernandez@pioix.edu.ar>
2025-07-05 14:44:14 -0300

    Backend del Admin
    Añadido de funciones a los botones de Admin HTML
    
    Adaptaciones de maneras de conseguir los ids y comparaciones de si esta vacio ("" -> undefined)
    
    Modificacion de IDs para llamarlos en Javascript
    
    Nueva Instanciacion de clase

--- frontend/js/dom.js
+++ frontend/js/dom.js
@@ -1,4 +1,4 @@
-class ui {
+class UI {
     constructor(){
 
     }
@@ -30,5 +30,86 @@ class ui {
         return usuario
     }
 
+    getCategoryAdd(){
+        let usuario = document.getElementById("select").value
+        return usuario
+    }
+
+    getContent(){
+        let usuario = document.getElementById("contenido").value
+        return usuario
+    }
+
+    getOpcion1(){
+        let usuario = document.getElementById("opcion-1").value
+        return usuario
+    }
+
+    getOpcion2(){
+        let usuario = document.getElementById("opcion-2").value
+        return usuario
+    }
+
+    getOpcion3(){
+        let usuario = document.getElementById("opcion-3").value
+        return usuario
+    }
+
+    getOpcion4(){
+        let usuario = document.getElementById("opcion-4").value
+        return usuario
+    }
+
+    getCategoryMod(){
+        let usuario = document.getElementById("select2").value
+        return usuario
+    }
+
+    getModPreg(){
+        let usuario = document.getElementById("preguntaid-2").value
+        return usuario
+    }
+
+    getContentMod(){
+        let usuario = document.getElementById("contenido2").value
+        return usuario
+    }
+
+    getOpcion1M(){
+        let usuario = document.getElementById("opcion-1-2").value
+        return usuario
+    }
+
+    getOpcion2M(){
+        let usuario = document.getElementById("opcion-2-2").value
+        return usuario
+    }
+
+    getOpcion3M(){
+        let usuario = document.getElementById("opcion-3-2").value
+        return usuario
+    }
+
+    getOpcion4M(){
+        let usuario = document.getElementById("opcion-4-2").value
+        return usuario
+    }
+
+    getElimUser(){
+        let usuario = document.getElementById("usuarioid-1").value
+        return usuario
+    }
+
+    getElimPoints(){
+        let usuario = document.getElementById("usuarioid-2").value
+        return usuario
+    }
+    
+    getElimPreg(){
+        let usuario = document.getElementById("preguntaid-1").value
+        return usuario
+    }
 }
+
+const ui = new UI();
 
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -20,7 +20,7 @@ function registrar() {
     user = ui.getUsername();
     mail = ui.getMail();
     password = ui.getPassword();
-    if(user == "" || mail == "" || password == ""){
+    if(user == undefined || mail == undefined || password == undefined){
         return ui.showModal("Error", "Faltan datos")
     }
     let datos = {
@@ -50,12 +50,12 @@ async function login(datos) {
 
 
 function loguear() {
-    user = ui.getUsername();
-    password = ui.getPassword();
-    if(user == "" || password == ""){
+    let user = ui.getUsername();
+    let password = ui.getPassword();
+    if(user == undefined || password == undefined){
         return ui.showModal("Error", "Faltan datos")
     } else if(user == "admin" && password == "juegoCeleste"){
-        window.location.href = '' //Insertar HTML admin;
+        window.location.replace("admin.html")
     }
     let datos = {
         username: user,
@@ -82,8 +82,8 @@ async function deleteUsers(datos) {
 
 
 function borrarUsuario() {
-    user = ui.getUsername();
-    if(user == ""){
+    user = ui.getElimUser();
+    if(user == undefined){
         return ui.showModal("Error", "Faltan datos")
     }
     let datos = {
@@ -95,7 +95,7 @@ function borrarUsuario() {
 
 async function sumarPregunta(datos) {
     response = await fetch(`http://localhost:4000/preguntas`,{
-        method:"DELETE", 
+        method:"POST", 
         headers: {
             "Content-Type": "application/json",
           },
@@ -103,9 +103,12 @@ async function sumarPregunta(datos) {
     })
     let result = await response.json()
     if(result.validar == false){
-        return ui.showModal("Error", "Pregunta no creada; verifique los campos ingresados")
+        ui.showModal("Error", "Pregunta no creada; verifique los campos ingresados")
+        return;
     } else {
         ui.showmodal("Exito", "Pregunta Añadida")
+        let pregId = result.pregunta
+        return pregId
     }
 }
 
@@ -113,14 +116,15 @@ async function sumarPregunta(datos) {
 function aniadirPregunta() {
     user = ui.getUsername();
     mail = ui.getMail();
-    if(user == "" || mail == ""){
+    if(user == undefined || mail == undefined){
         return ui.showModal("Error", "Faltan datos")
     }
     let datos = {
         pregunta: user,
         id: mail,
     }
-    sumarPregunta(datos)
+    let pregId = sumarPregunta(datos)
+    return pregId
 }
 
 async function cambiazoPregunta(datos) {
@@ -141,9 +145,9 @@ async function cambiazoPregunta(datos) {
 
 
 function actualizarPregunta() {
-    user = ui.getUsername();
-    mail = ui.getMail();
-    if(user == "" || mail == ""){
+    user = ui.getContent();
+    mail = ui.getCategotyAdd();
+    if(user == undefined || mail == undefined){
         return ui.showModal("Error", "Faltan datos")
     }
     let datos = {
@@ -165,20 +169,20 @@ async function deletePregunta(datos) {
     if(result.validar == false){
         return ui.showModal("Error", "Pregunta no eliminada; verifique los campos ingresados o intente de nuevo mas tarde")
     } else {
-        ui.showmodal("Exito", "Pregunta eliminada de la faz del juego")
+        ui.showmodal("Exito", "Pregunta eliminada de la faz del juego; restan opciones")
     }
 }
 
 
 function borrarPregunta() {
-    user = ui.getUsername();
-    if(user == ""){
+    user = ui.getElimPreg();
+    if(user == undefined){
         return ui.showModal("Error", "Faltan datos")
     }
     let datos = {
         id: user,
     }
-    deleteUsers(datos)
+    deletePregunta(datos)
 }
 
 async function sumarOpcion(datos) {
@@ -198,15 +202,13 @@ async function sumarOpcion(datos) {
 }
 
 
-function aniadirOpcion() {
-    user = ui.getUsername();
-    mail = ui.getMail();
-    if(user == "" || mail == ""){
+function aniadirOpcion(opcion, id) {
+    if(opcion == undefined || id == undefined){
         return ui.showModal("Error", "Faltan datos")
     }
     let datos = {
-        opcion: user,
-        id: mail,
+        opcion: opcion,
+        id: id,
     }
     sumarOpcion(datos)
 }
@@ -228,15 +230,13 @@ async function cambiazoOpcion(datos) {
 }
 
 
-function actualizarOpcion() {
-    user = ui.getUsername();
-    mail = ui.getMail();
-    if(user == "" || mail == ""){
+function actualizarOpcion(cambio, id) {
+    if(cambio == undefined || id == undefined){
         return ui.showModal("Error", "Faltan datos")
     }
     let datos = {
-        cambio: user,
-        id: mail,
+        cambio: cambio,
+        id: id,
     }
     cambiazoOpcion(datos)
 }
@@ -260,7 +260,7 @@ async function menosOpcion(datos) {
 
 function borrarOpcion() {
     user = ui.getUsername();
-    if(user == ""){
+    if(user == undefined){
         return ui.showModal("Error", "Faltan datos")
     }
     let datos = {
@@ -279,20 +279,70 @@ async function noMasOpcion(datos) {
     })
     let result = await response.json()
     if(result.validar == false){
-        return ui.showModal("Error", "Pregunta no eliminada; verifique los campos ingresados o intente de nuevo mas tarde")
+        return ui.showModal("Algo salio mal", "Opciones no eliminadas")
     } else {
-        ui.showmodal("Exito", "Pregunta eliminada de la faz del juego")
+        ui.showmodal("Exito", "Pregunta completamente terminada")
     }
 }
 
 
 function borrarOpciones() {
-    user = ui.getUsername();
-    if(user == ""){
+    user = ui.getElimPreg();
+    if(user == undefined){
         return ui.showModal("Error", "Faltan datos")
     }
     let datos = {
         id: user,
     }
     noMasOpcion(datos)
+}
+
+function botonAddPreg(){
+    let opcion1 = ui.getOpcion1();
+    let opcion2 = ui.getOpcion2();
+    let opcion3 = ui.getOpcion3();
+    let opcion4 = ui.getOpcion4();
+    let pregId = aniadirPregunta()
+    if(opcion1 != undefined){
+        aniadirOpcion(opcion1, pregId)
+    }
+    if(opcion2 != undefined){
+        aniadirOpcion(opcion2, pregId)
+    }
+    if(opcion3 != undefined){
+        aniadirOpcion(opcion3, pregId)
+    }
+    if(opcion4 != undefined){
+        aniadirOpcion(opcion4, pregId)
+    }
+}
+
+function botonElimPreg(){
+  borrarPregunta();
+  borrarOpciones();  
+}
+
+function botonModPreg(){
+    let pregId = ui.getModPreg()
+    let o1Id = pregId*4-3;
+    let o2Id = pregId*4-2;
+    let o3Id = pregId*4-1;
+    let o4Id = pregId*4;
+    let opcion1 = ui.getOpcion1();
+    let opcion2 = ui.getOpcion2();
+    let opcion3 = ui.getOpcion3();
+    let opcion4 = ui.getOpcion4();
+    actualizarPregunta();
+    if(opcion1 != undefined){
+        actualizarOpcion(opcion1, o1Id)
+    }
+    if(opcion2 != undefined){
+        actualizarOpcion(opcion2, o2Id)
+    }
+    if(opcion3 != undefined){
+        actualizarOpcion(opcion3, o3Id)
+    }
+    if(opcion4 != undefined){
+        actualizarOpcion(opcion4, o4Id)
+    }
 }
